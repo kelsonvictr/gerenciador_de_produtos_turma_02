5e0004766fc7f7b41c37ffc37e7ce8c249daceb3
Kelson Ameida <kelson.victr@gmail.com>
2024-07-27 16:02:20 -0300

    Aula 6 - Andamento

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import FornecedorList from './pages/Fornecedor/FornecedorList'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
+    <Navbar />
+    <div className="container">
+    <Routes>
         <Route path="/" element={<FornecedorList />} />
         <Route path="/listar-fornecedores" element={<FornecedorList />} />
       </Routes>
+    </div>
     </BrowserRouter>
   )
 }
