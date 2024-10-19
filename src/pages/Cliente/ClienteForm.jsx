42383a0785320c12c52d7c4222371f0bc9e54ec7
kelsonalmeida <kelson.victr@gmail.com>
2024-10-19 13:22:12 -0300

    ClienteForm, FornecedorForm, ProdutoForm, ProdutoList

--- src/pages/Cliente/ClienteForm.jsx
+++ src/pages/Cliente/ClienteForm.jsx
@@ -3,7 +3,7 @@ import { useNavigate, useParams } from 'react-router-dom'
 import axios from '../../api'
 import InputMask from 'react-input-mask'
 import Modal from 'react-modal'
-import { FaCheckCircle, FaQuestionCircle } from 'react-icons/fa'
+import { FaCheckCircle, FaExclamationTriangle, FaQuestionCircle } from 'react-icons/fa'
 
 Modal.setAppElement('#root')
 
@@ -12,15 +12,20 @@ const ClienteForm = () => {
     nome: '',
     cpf: '',
     email: '',
-    cep: '',
-    rua: '',
-    numero: '',
-    bairro: '',
-    cidade: '',
-    estado: '',
-    pais: 'Brasil',
+    endereco: {
+      logradouro: '',
+      numero: '',
+      complemento: '',
+      bairro: '',
+      cidade: '',
+      estado: '',
+      pais: 'Brasil',
+      cep: '',
+    }
   })
   const [modalAberto, setModalAberto] = useState(false)
+  const [modalErroAberto, setModalErroAberto] = useState(false)
+  const [mensagensErro, setMensagensErro] = useState([])
   const [tooltipAberto, setTooltipAberto] = useState(false)
   const navigate = useNavigate()
   const { id } = useParams()
@@ -32,30 +37,67 @@ const ClienteForm = () => {
           setCliente(response.data)
         })
         .catch(error => console.error("Ocorreu um erro: ", error))
+    } else {
+      // Se não houver ID, redefinir o estado do cliente para adicionar um novo
+      setCliente({
+        nome: '',
+        cpf: '',
+        email: '',
+        endereco: {
+          logradouro: '',
+          numero: '',
+          complemento: '',
+          bairro: '',
+          cidade: '',
+          estado: '',
+          pais: 'Brasil',
+          cep: ''
+        }
+      })
     }
   }, [id])
 
   const handleSubmit = (event) => {
     event.preventDefault()
+    setMensagensErro([]) // Limpa erros anteriores
 
-    if (id) {
-      axios.put(`/clientes/${id}`, cliente)
-        .then(() => {
-          setModalAberto(true)
-        })
-        .catch(error => console.error("Ocorreu um erro: ", error))
-    } else {
-      axios.post('/clientes', cliente)
-        .then(() => {
-          setModalAberto(true)
-        })
-        .catch(error => console.error("Ocorreu um erro: ", error))
+    // Preparar dados para envio
+    const clienteData = {
+      ...cliente,
+      cpf: cliente.cpf.replace(/[^\d]/g, ''),
+      endereco: {
+        ...cliente.endereco,
+        cep: cliente.endereco.cep.replace(/[^\d]/g, ''),
+      }
     }
+
+    const request = id 
+      ? axios.put(`/clientes/${id}`, clienteData)
+      : axios.post('/clientes', clienteData)
+
+    request.then(() => {
+      setModalAberto(true)
+    })
+    .catch(error => {
+      if (error.response && error.response.status === 500) {
+        setMensagensErro(["Erro no sistema, entre em contato com o administrador"])
+        setModalErroAberto(true)
+      } else if (error.response && error.response.data) {
+        setMensagensErro(Object.values(error.response.data))
+        setModalErroAberto(true)
+      } else {
+        console.error("Ocorreu um erro: ", error)
+      }
+    })
   }
 
   const fecharModal = () => {
     setModalAberto(false)
     navigate("/listar-clientes")
+  }
+
+  const fecharModalErro = () => {
+    setModalErroAberto(false)
   }
 
   const adicionarOutroCliente = () => {
@@ -64,13 +106,16 @@ const ClienteForm = () => {
       nome: '',
       cpf: '',
       email: '',
-      cep: '',
-      rua: '',
-      numero: '',
-      bairro: '',
-      cidade: '',
-      estado: '',
-      pais: 'Brasil',
+      endereco: {
+        logradouro: '',
+        numero: '',
+        complemento: '',
+        bairro: '',
+        cidade: '',
+        estado: '',
+        pais: 'Brasil',
+        cep: ''
+      }
     })
   }
 
@@ -80,7 +125,10 @@ const ClienteForm = () => {
 
   const handleCepChange = (e) => {
     const cep = e.target.value.replace(/\D/g, '')
-    setCliente({ ...cliente, cep: e.target.value })
+    setCliente({ 
+      ...cliente, 
+      endereco: { ...cliente.endereco, cep: e.target.value }
+    })
 
     if (cep.length === 8) {
       axios.get(`https://viacep.com.br/ws/${cep}/json/`)
@@ -88,10 +136,13 @@ const ClienteForm = () => {
           if (!response.data.erro) {
             setCliente(prevCliente => ({
               ...prevCliente,
-              rua: response.data.logradouro,
-              bairro: response.data.bairro,
-              cidade: response.data.localidade,
-              estado: response.data.uf
+              endereco: {
+                ...prevCliente.endereco,
+                logradouro: response.data.logradouro,
+                bairro: response.data.bairro,
+                cidade: response.data.localidade,
+                estado: response.data.uf
+              }
             }))
           }
         })
@@ -162,20 +213,23 @@ const ClienteForm = () => {
             className="form-control" 
             id="cep" 
             name="cep" 
-            value={cliente.cep} 
+            value={cliente.endereco.cep} 
             onChange={handleCepChange} 
             required 
           />
         </div>
         <div className="form-group">
-          <label htmlFor="rua">Rua:</label>
+          <label htmlFor="logradouro">Logradouro:</label>
           <input 
             type="text" 
             className="form-control" 
-            id="rua" 
-            name="rua" 
-            value={cliente.rua} 
-            onChange={e => setCliente({ ...cliente, rua: e.target.value })} 
+            id="logradouro" 
+            name="logradouro" 
+            value={cliente.endereco.logradouro} 
+            onChange={e => setCliente({ 
+              ...cliente, 
+              endereco: { ...cliente.endereco, logradouro: e.target.value }
+            })} 
             required 
           />
         </div>
@@ -186,9 +240,26 @@ const ClienteForm = () => {
             className="form-control" 
             id="numero" 
             name="numero" 
-            value={cliente.numero} 
-            onChange={e => setCliente({ ...cliente, numero: e.target.value })} 
+            value={cliente.endereco.numero} 
+            onChange={e => setCliente({ 
+              ...cliente, 
+              endereco: { ...cliente.endereco, numero: e.target.value }
+            })} 
             required 
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="complemento">Complemento:</label>
+          <input 
+            type="text" 
+            className="form-control" 
+            id="complemento" 
+            name="complemento" 
+            value={cliente.endereco.complemento} 
+            onChange={e => setCliente({ 
+              ...cliente, 
+              endereco: { ...cliente.endereco, complemento: e.target.value }
+            })} 
           />
         </div>
         <div className="form-group">
@@ -198,8 +269,11 @@ const ClienteForm = () => {
             className="form-control" 
             id="bairro" 
             name="bairro" 
-            value={cliente.bairro} 
-            onChange={e => setCliente({ ...cliente, bairro: e.target.value })} 
+            value={cliente.endereco.bairro} 
+            onChange={e => setCliente({ 
+              ...cliente, 
+              endereco: { ...cliente.endereco, bairro: e.target.value }
+            })} 
             required 
           />
         </div>
@@ -210,8 +284,11 @@ const ClienteForm = () => {
             className="form-control" 
             id="cidade" 
             name="cidade" 
-            value={cliente.cidade} 
-            onChange={e => setCliente({ ...cliente, cidade: e.target.value })} 
+            value={cliente.endereco.cidade} 
+            onChange={e => setCliente({ 
+              ...cliente, 
+              endereco: { ...cliente.endereco, cidade: e.target.value }
+            })} 
             required 
           />
         </div>
@@ -222,8 +299,11 @@ const ClienteForm = () => {
             className="form-control" 
             id="estado" 
             name="estado" 
-            value={cliente.estado} 
-            onChange={e => setCliente({ ...cliente, estado: e.target.value })} 
+            value={cliente.endereco.estado} 
+            onChange={e => setCliente({ 
+              ...cliente, 
+              endereco: { ...cliente.endereco, estado: e.target.value }
+            })} 
             required 
           />
         </div>
@@ -234,27 +314,50 @@ const ClienteForm = () => {
             className="form-control" 
             id="pais" 
             name="pais" 
-            value={cliente.pais} 
-            onChange={e => setCliente({ ...cliente, pais: e.target.value })} 
+            value={cliente.endereco.pais} 
+            onChange={e => setCliente({ 
+              ...cliente, 
+              endereco: { ...cliente.endereco, pais: e.target.value }
+            })} 
             required 
           />
         </div>
+
         <button type="submit" className="btn-success">{id ? 'Salvar' : 'Adicionar'}</button>
       </form>
 
+      {/* Modal de sucesso */}
       <Modal
         isOpen={modalAberto}
         onRequestClose={fecharModal}
         className="modal"
         overlayClassName="overlay"
       >
-        <div className="modal-content">
-          <FaCheckCircle className="icon" />
+        <div className="modalContent">
+          <FaCheckCircle className="icon successIcon" />
           <h2>{id ? 'Cliente atualizado com sucesso!' : 'Cliente adicionado com sucesso!'}</h2>
-          <div className="modal-buttons">
+          <div className="modalButtons">
             <button onClick={fecharModal} className="btn-success">Fechar</button>
             {!id && <button onClick={adicionarOutroCliente} className="btn-secondary">Adicionar outro cliente</button>}
           </div>
+        </div>
+      </Modal>
+
+      {/* Modal de erro */}
+      <Modal
+        isOpen={modalErroAberto}
+        onRequestClose={fecharModalErro}
+        className="modal"
+        overlayClassName="overlay"
+      >
+        <div className="modalContent">
+          <FaExclamationTriangle className="icon errorIcon" />
+          <h2>Ocorreu um erro:</h2>
+          {mensagensErro.map((mensagem, index) => (
+            <h4 key={index}>{mensagem}</h4>
+          ))}
+          <br />
+          <button onClick={fecharModalErro} className="btn-secondary">Fechar</button>
         </div>
       </Modal>
     </div>
--- src/pages/Produto/ProdutoForm.jsx
+++ src/pages/Produto/ProdutoForm.jsx
@@ -2,43 +2,85 @@ import React, { useState, useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import axios from '../../api'
 import Modal from 'react-modal'
-import { FaCheckCircle, FaQuestionCircle } from 'react-icons/fa'
+import { FaCheckCircle, FaExclamationTriangle, FaQuestionCircle } from 'react-icons/fa'
 
 Modal.setAppElement('#root')
 
 const ProdutoForm = () => {
-  const [produto, setProduto] = useState({ nome: '', preco: '', descricao: '', quantidadeEstoque: '' })
+  const [produto, setProduto] = useState({ nome: '', preco: '', descricao: '', quantidadeEstoque: '', fornecedorId: '' })
+  const [fornecedores, setFornecedores] = useState([])
   const [modalAberto, setModalAberto] = useState(false)
+  const [modalErroAberto, setModalErroAberto] = useState(false)
+  const [mensagensErro, setMensagensErro] = useState([])
   const [tooltipAberto, setTooltipAberto] = useState(false)
   const navigate = useNavigate()
   const { id } = useParams()
 
   useEffect(() => {
+    // Buscar fornecedores
+    axios.get('/fornecedores')
+      .then(response => {
+        setFornecedores(response.data)
+      })
+      .catch(error => console.error("Erro ao buscar fornecedores: ", error))
+
+    // Se houver um ID, buscar os dados do produto
     if (id) {
       axios.get(`/produtos/${id}`)
         .then(response => {
-          setProduto(response.data)
+          setProduto({
+            ...response.data,
+            fornecedorId: response.data.fornecedor ? response.data.fornecedor.id : ''
+          })
         })
         .catch(error => console.error("Ocorreu um erro: ", error))
+    } else {
+      // Se não houver ID, redefinir o estado do produto para adicionar um novo
+      setProduto({ nome: '', preco: '', descricao: '', quantidadeEstoque: '', fornecedorId: '' })
     }
   }, [id])
 
   const handleSubmit = (event) => {
     event.preventDefault()
+    setMensagensErro([]) // Limpa erros anteriores
 
-    if (id) {
-      axios.put(`/produtos/${id}`, produto)
-        .then(() => {
-          setModalAberto(true)
-        })
-        .catch(error => console.error("Ocorreu um erro: ", error))
-    } else {
-      axios.post('/produtos', produto)
-        .then(() => {
-          setModalAberto(true)
-        })
-        .catch(error => console.error("Ocorreu um erro: ", error))
+    // Garantir que o preço esteja no formato correto antes de enviar
+    const precoFormatado = parseFloat(produto.preco).toFixed(2)
+    const produtoData = { ...produto, preco: precoFormatado }
+
+    const request = id 
+      ? axios.put(`/produtos/${id}`, produtoData)
+      : axios.post('/produtos', produtoData)
+
+    request.then(() => {
+      setModalAberto(true)
+    }).catch(error => {
+      if (error.response && error.response.data) {
+        setMensagensErro(Object.values(error.response.data))
+        setModalErroAberto(true)
+      } else {
+        console.error("Ocorreu um erro: ", error)
+      }
+    })
+  }
+
+  const handlePrecoChange = (event) => {
+    let valor = event.target.value
+
+    // Substituir vírgula por ponto
+    valor = valor.replace(',', '.')
+
+    // Remover caracteres que não sejam dígitos ou ponto
+    valor = valor.replace(/[^0-9.]/g, '')
+
+    // Garantir que tenha no máximo um ponto e duas casas decimais
+    if (valor.includes('.')) {
+      const [parteInteira, parteDecimal] = valor.split('.')
+      valor = parteInteira + '.' + (parteDecimal ? parteDecimal.slice(0, 2) : '')
     }
+
+    // Atualizar o estado do produto com o valor formatado
+    setProduto({ ...produto, preco: valor })
   }
 
   const fecharModal = () => {
@@ -46,9 +88,13 @@ const ProdutoForm = () => {
     navigate("/listar-produtos")
   }
 
+  const fecharModalErro = () => {
+    setModalErroAberto(false)
+  }
+
   const adicionarOutroProduto = () => {
     setModalAberto(false)
-    setProduto({ nome: '', preco: '', descricao: '', quantidadeEstoque: '' })
+    setProduto({ nome: '', preco: '', descricao: '', quantidadeEstoque: '', fornecedorId: '' })
   }
 
   const toggleTooltip = () => {
@@ -88,13 +134,12 @@ const ProdutoForm = () => {
         <div className="form-group">
           <label htmlFor="preco">Preço do produto:</label>
           <input 
-            type="number" 
-            step="0.01"
+            type="text" 
             className="form-control" 
             id="preco" 
             name="preco" 
             value={produto.preco} 
-            onChange={e => setProduto({ ...produto, preco: e.target.value })} 
+            onChange={handlePrecoChange}
             required 
           />
         </div>
@@ -122,9 +167,28 @@ const ProdutoForm = () => {
             required 
           />
         </div>
+        <div className="form-group">
+          <label htmlFor="fornecedorId">Fornecedor:</label>
+          <select
+            className="form-control"
+            id="fornecedorId"
+            name="fornecedorId"
+            value={produto.fornecedorId}
+            onChange={e => setProduto({ ...produto, fornecedorId: e.target.value })}
+            required
+          >
+            <option value="">Selecione um fornecedor</option>
+            {fornecedores.map(fornecedor => (
+              <option key={fornecedor.id} value={fornecedor.id}>
+                {fornecedor.nome}
+              </option>
+            ))}
+          </select>
+        </div>
         <button type="submit" className="btn-success">{id ? 'Salvar' : 'Adicionar'}</button>
       </form>
 
+      {/* Modal de sucesso */}
       <Modal
         isOpen={modalAberto}
         onRequestClose={fecharModal}
@@ -138,6 +202,23 @@ const ProdutoForm = () => {
             <button onClick={fecharModal} className="btn-success">Fechar</button>
             {!id && <button onClick={adicionarOutroProduto} className="btn-secondary">Adicionar outro produto</button>}
           </div>
+        </div>
+      </Modal>
+
+      {/* Modal de erro */}
+      <Modal
+        isOpen={modalErroAberto}
+        onRequestClose={fecharModalErro}
+        className="modal"
+        overlayClassName="overlay"
+      >
+        <div className="modalContent">
+          <FaExclamationTriangle className="icon errorIcon" />
+          <h2>Ocorreu um erro:</h2>
+          {mensagensErro.map((mensagem, index) => (
+            <h4 key={index}>{mensagem}</h4>
+          ))}
+          <button onClick={fecharModalErro} className="btn-secondary">Fechar</button>
         </div>
       </Modal>
     </div>
