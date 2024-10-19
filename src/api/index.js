90fe728f7f7f4d71eaf2070e8bb326422a181939
kelsonalmeida <kelson.victr@gmail.com>
2024-10-19 10:54:23 -0300

    index.js atualizado

--- src/api/index.js
+++ src/api/index.js
@@ -4,4 +4,30 @@ const instance = axios.create({
     baseURL: 'http://localhost:8080'
 })
 
+// Adiciona o token em cada requisição, se ele estiver presente
+// Adiciona o token em cada requisição, se ele estiver presente
+instance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token')
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`
+        }
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+// Redireciona para o login se receber um 403
+instance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && error.response.status === 403) {
+            window.location.href = '/login'
+        }
+        return Promise.reject(error)
+    }
+)
+
 export default instance
