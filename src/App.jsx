1427f9769873413200b139f42742501a331914f8
kelsonalmeida <kelson.victr@gmail.com>
2024-10-19 13:31:35 -0300

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,33 @@ import ProdutoForm from './pages/Produto/ProdutoForm'
 import Inicial from './pages/Inicial'
 import ClienteList from './pages/Cliente/ClienteList'
 import ClienteForm from './pages/Cliente/ClienteForm'
+import LoginPage from './pages/Auth/LoginPage'
+import RegisterPage from './pages/Auth/RegisterPage'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Navbar />
-    <div className="container">
-    <Routes>
-        <Route path="/" element={<Inicial />} />
-        <Route path="/listar-fornecedores" element={<FornecedorList />} />
-        <Route path="/add-fornecedores" element={<FornecedorForm />} />
-        <Route path="/edit-fornecedores/:id" element={<FornecedorForm />} />
-        <Route path="/listar-produtos" element={<ProdutoList />} />
-        <Route path="/add-produtos" element={<ProdutoForm />} />
-        <Route path="/add-clientes" element={<ClienteForm />} />
-        <Route path="/listar-clientes" element={<ClienteList />} />
-        <Route path="/edit-clientes/:id" element={<ClienteForm />} />
-      </Routes>
-    </div>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          {/* Rotas Públicas */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          
+          {/* Rotas Privadas */}
+          <Route path="/" element={<PrivateRoute><Inicial /></PrivateRoute>} />
+          <Route path="/listar-fornecedores" element={<PrivateRoute><FornecedorList /></PrivateRoute>} />
+          <Route path="/add-fornecedores" element={<PrivateRoute><FornecedorForm /></PrivateRoute>} />
+          <Route path="/edit-fornecedores/:id" element={<PrivateRoute><FornecedorForm /></PrivateRoute>} />
+          <Route path="/listar-produtos" element={<PrivateRoute><ProdutoList /></PrivateRoute>} />
+          <Route path="/edit-produtos/:id" element={<PrivateRoute><ProdutoForm /></PrivateRoute>} />
+          <Route path="/add-produtos" element={<PrivateRoute><ProdutoForm /></PrivateRoute>} />
+          <Route path="/add-clientes" element={<PrivateRoute><ClienteForm /></PrivateRoute>} />
+          <Route path="/listar-clientes" element={<PrivateRoute><ClienteList /></PrivateRoute>} />
+          <Route path="/edit-clientes/:id" element={<PrivateRoute><ClienteForm /></PrivateRoute>} />
+        </Routes>
+      </div>
     </BrowserRouter>
   )
 }
